b9f8d369036a4c2a6c1ff44d79b79d4212397021
herr kaste <herr.kaste@gmail.com>
2023-12-31 00:41:51 +0100

    Extract `ensure_window`

--- plugin.py
+++ plugin.py
@@ -1,11 +1,12 @@
-
+from functools import wraps
 
 import sublime
 import sublime_plugin
 
 
-from typing import Dict, List, Tuple, TypedDict
+from typing import Callable, Dict, List, Tuple, TypedDict, TypeVar
 FrozenSel = List[Tuple[int, int]]
+T = TypeVar("T", bound=sublime_plugin.EventListener)
 
 
 class ViewState(TypedDict):
@@ -17,15 +18,26 @@ Filename = str
 Store = Dict[Filename, ViewState]
 
 
-class ReopenFilesListener(sublime_plugin.EventListener):
-    def on_pre_close(self, view: sublime.View):
+def ensure_window(
+    handler: Callable[[T, sublime.View, sublime.Window], None]
+) -> Callable[[T, sublime.View], None]:
+    @wraps(handler)
+    def wrapper(self: T, view: sublime.View) -> None:
         window = view.window()
-        # If the window was closed, we receive `on_pre_close` for each
-        # view but all are detached.  Which is good as Sublime will
-        # reload the project with all views ("hot_exit") anyway.
         if not window:
             return
+        handler(self, view, window)
+    return wrapper
 
+
+class ReopenFilesListener(sublime_plugin.EventListener):
+    # If the window was closed, we receive `on_pre_close` for each
+    # view but all are detached hence `ensure_window`.
+    # This is actually good behavior as Sublime will reload
+    # the project with all views ("hot_exit") anyway so there is
+    # no need to persist it here too.
+    @ensure_window
+    def on_pre_close(self, view: sublime.View, window: sublime.Window) -> None:
         file_name = view.file_name()
         if not file_name:
             return
@@ -38,14 +50,11 @@ class ReopenFilesListener(sublime_plugin.EventListener):
         store[file_name] = state
         window.settings().set("rf_store", store)
 
+    @ensure_window
     # Use `on_load_async` here and check if the viewport or cursor
     # has been moved already to not override a possible
     # `window.open_file()` with a `row:col` pair.
-    def on_load_async(self, view: sublime.View):
-        window = view.window()
-        if not window:
-            return
-
+    def on_load_async(self, view: sublime.View, window: sublime.Window) -> None:
         if (
             view.viewport_position() != (0, 0)
             or freeze_sel(view) != [(0, 0)]
